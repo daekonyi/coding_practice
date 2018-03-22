b12ef29f9c9e8a0c4b9597577e6fc0c2eb92246b
Dae Yi <daekonyi@gmail.com>
2018-03-21 22:14:51 -0400

    changed random_practice

--- random_practice/code.cpp
+++ random_practice/code.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	random_device rd;
-	mt19937 mt(rd());
+	default_random_engine engine(rd());
 	uniform_int_distribution<int> dist(0, 10);
 	
 	for (int i=0; i<16; ++i) {
-		cout << dist(mt) << endl;
+		cout << dist(engine) << endl;
 	}
 	return 0;
 }
